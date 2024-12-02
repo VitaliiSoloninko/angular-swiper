145daa4708885013cfaa9dba673297c5334343cd
22051988Di <vitalii.soloninko@gmail.com>
2024-12-02 17:11:42 +0100

    1.3	First slider

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { register as registerSwiperElements } from 'swiper/element/bundle';
+import { register } from 'swiper/element/bundle';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-registerSwiperElements();
+register();
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
