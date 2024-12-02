c6c43f4cf1add5e9615ff7935707decd249f4709
22051988Di <vitalii.soloninko@gmail.com>
2024-12-02 17:02:38 +0100

    1.1	Import custom schema

---
+++ src/app/swiper-basic/swiper-basic.component.ts
@@ -0,0 +1,11 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-swiper-basic',
+  standalone: true,
+  imports: [],
+  templateUrl: './swiper-basic.component.html',
+  styleUrl: './swiper-basic.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class SwiperBasicComponent {}
